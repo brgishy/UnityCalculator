7a3945838ef990d34bdc2c3d7a18f257a8227668
Brian Gish <gish.brian@gmail.com>
2023-09-28 10:05:43 -0700

    Moving all the project files into their own directory to clean up the root

--- Publish NativeAOT - Windows.bat
+++ Publish NativeAOT - Windows.bat
@@ -1,13 +1,15 @@
 
 @ECHO OFF
 
+CD UnityCalculator
+
 :: Making sure library files exist, and download if they do not
 IF NOT EXIST ".\Native\win-x64" MKDIR ".\Native\win-x64"
 
-IF NOT EXIST .\Native\win-x64\libHarfBuzzSharp.lib ECHO Downloading libHarfBuzzSharp.lib...
+IF NOT EXIST .\Native\win-x64\libHarfBuzzSharp.lib ECHO Downloading libHarfBuzzSharp.lib (Approx 105 MB)...
 IF NOT EXIST .\Native\win-x64\libHarfBuzzSharp.lib powershell -Command "Invoke-WebRequest https://github.com/brgishy/StaticFiles/raw/main/AvaloniaLibs/win-x64/libHarfBuzzSharp.lib?download= -Outfile .\Native\win-x64\libHarfBuzzSharp.lib"
 
-IF NOT EXIST .\Native\win-x64\libSkiaSharp.lib ECHO Downloading libSkiaSharp.lib...
+IF NOT EXIST .\Native\win-x64\libSkiaSharp.lib ECHO Downloading libSkiaSharp.lib (Approx 392 MB)...
 IF NOT EXIST .\Native\win-x64\libSkiaSharp.lib powershell -Command "Invoke-WebRequest https://github.com/brgishy/StaticFiles/raw/main/AvaloniaLibs/win-x64/libSkiaSharp.lib?download= -Outfile .\Native\win-x64\libSkiaSharp.lib"
 
 :: Performing the Publish
@@ -28,6 +30,6 @@ dotnet publish ^
 
 :: Removing unecessary files and copying the output to the root folder
 DEL  .\bin\%CONFIG%\net7.0\%RUNTIME%\publish\*.dll
-MOVE .\bin\%CONFIG%\net7.0\%RUNTIME%\publish\*.exe .
+MOVE .\bin\%CONFIG%\net7.0\%RUNTIME%\publish\*.exe ..
 
 PAUSE
